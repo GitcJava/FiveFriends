5064d69c6a2e4c81c9f041bc57581481e394c99a
Karen Vardanyan <karenvardanyan@workfront.com>
2016-04-11 16:09:34 +0400

    initial

---
+++ src/hanoitowers/TowerCanvas.java
@@ -0,0 +1,23 @@
+package hanoitowers;
+
+import java.awt.*;
+
+
+public class TowerCanvas {
+
+
+    public TowerCanvas(){
+        //TODO
+    }
+
+    public void addDisc(int c) {
+        //TODO
+    }
+    public void reset(){
+        //TODO
+    }
+
+    public void play(){
+        //TODO
+    }
+}
---
+++ src/hanoitowers/TowerFrame.java
@@ -0,0 +1,15 @@
+package hanoitowers;
+
+/**
+ * Created by Gor_Gevorgyan on 08.04.2016.
+ */
+public class TowerFrame {
+
+    public TowerFrame(){
+
+    }
+
+    public static void main(String[] args) {
+        TowerFrame towerFrame = new TowerFrame();
+    }
+}
