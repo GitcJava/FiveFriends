3b4674050baa0dba2d749e4aa7708faf3f9dcf58
gevorgyangor <gevorgyan604@gmail.com>
2016-04-14 21:32:06 +0400

    add

--- src/Peg.java
+++ src/Peg.java
@@ -2,14 +2,107 @@ import figures.Figure;
 import figures.FigureCanvas;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 
 public class Peg extends Figure {
 
+    ArrayList<Figure> discs = new ArrayList<Figure>();
+    private int xBottom;
+    private int yBottom;
+    private int widthBottom;
+    private int heightBottom;
+    private TowerCanvas towerCanvas;
 
-    protected Peg(int x, int y, int width, int height, FigureCanvas figureCanvas, Color color) {
-        super(x, y, width, height, figureCanvas, color);
+
+    public Peg(int x, int y, int width, int height, int xBottom, int yBottom, int widthBottom, int heightBottom, TowerCanvas towerCanvas, Color color) {
+        super(x, y, width, height, color);
+
+        this.xBottom = xBottom;
+        this.yBottom = yBottom;
+        this.widthBottom = widthBottom;
+        this.heightBottom = heightBottom;
+        this.towerCanvas = towerCanvas;
     }
+    public void moveo(Peg to){
+        moveUp(to);
+
+        moveDown(to);
+    }
+
+    private void moveDown(Peg to){
+        while (isDoneDown(to)){
+            to.getLast().move(0, 4);
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            towerCanvas.repaint();
+        }
+    }
+    public boolean isDoneDown(Peg to){
+        if (to.discs.size() < 2){
+            return to.getLast().getY() < to.getyBottom() - to.getLast().getHeight();
+        }else {
+            return to.getLast().getY() < to.discs.get(to.discs.size() - 2).getY() - to.getLast().getHeight();
+        }
+    }
+
+    private void moveLeftOrRight(Peg to){
+
+    }
+    private Figure getLast() {
+        return discs.get(discs.size() - 1);
+    }
+
+
+    private void moveUp(Peg to) {
+        while (getLast().getY() > getY() - 50) {
+            getLast().move(0, -5);
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            towerCanvas.repaint();
+        }
+        to.discs.add(discs.remove(discs.size() - 1));
+    }
+
+
+    public int getxBottom() {
+        return xBottom;
+    }
+
+    public void setxBottom(int xBottom) {
+        this.xBottom = xBottom;
+    }
+
+    public int getyBottom() {
+        return yBottom;
+    }
+
+    public void setyBottom(int yBottom) {
+        this.yBottom = yBottom;
+    }
+
+    public int getWidthBottom() {
+        return widthBottom;
+    }
+
+    public void setWidthBottom(int widthBottom) {
+        this.widthBottom = widthBottom;
+    }
+
+    public int getHeightBottom() {
+        return heightBottom;
+    }
+
+    public void setHeightBottom(int heightBottom) {
+        this.heightBottom = heightBottom;
+    }
+
 
     @Override
     public boolean isBelong(int i, int i1) {
