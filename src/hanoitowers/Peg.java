5472f633ba7c0ea8d39d3d87521604739337efc4
Grigor <grigorabroyan@gmail.com>
2016-04-15 22:12:13 +0400

    add hanoiTower

--- src/hanoitowers/Peg.java
+++ src/hanoitowers/Peg.java
@@ -2,16 +2,65 @@ package hanoitowers;
 
 
 import figures.Figure;
-import figures.FigureCanvas;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 
 public class Peg extends Figure {
 
-    protected Peg(int x, int y, int width, int height, FigureCanvas figureCanvas, Color color) {
-        super(x, y, width, height, figureCanvas, color);
+
+    ArrayList<Figure> discs = new ArrayList<Figure>();
+
+    private int xBottom;
+    private int yBottom;
+    private int widthBottom;
+    private int heightBottom;
+    private TowerCanvas towerCanvas;
+
+
+    public Peg(int x, int y, int width, int height, int xBottom, int yBottom, int widthBottom, int heightBottom, TowerCanvas towerCanvas, Color color) {
+        super(x, y, width, height, color);
+        this.xBottom = xBottom;
+        this.yBottom = yBottom;
+        this.widthBottom = widthBottom;
+        this.heightBottom = heightBottom;
+        this.towerCanvas = towerCanvas;
     }
+
+
+    public int getxBottom() {
+        return xBottom;
+    }
+
+    public void setxBottom(int xBottom) {
+        this.xBottom = xBottom;
+    }
+
+    public int getyBottom() {
+        return yBottom;
+    }
+
+    public void setyBottom(int yBottom) {
+        this.yBottom = yBottom;
+    }
+
+    public int getWidthBottom() {
+        return widthBottom;
+    }
+
+    public void setWidthBottom(int widthBottom) {
+        this.widthBottom = widthBottom;
+    }
+
+    public int getHeightBottom() {
+        return heightBottom;
+    }
+
+    public void setHeightBottom(int heightBottom) {
+        this.heightBottom = heightBottom;
+    }
+
 
     @Override
     public boolean isBelong(int i, int i1) {
--- src/hanoitowers/TowerFrame.java
+++ src/hanoitowers/TowerFrame.java
@@ -1,15 +1,74 @@
 package hanoitowers;
 
 
-public class TowerFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class TowerFrame extends JFrame {
+ static JTextField jTextField;
+    TowerCanvas towerCanvas;
 
     public TowerFrame(){
+        super("Hanoi Tower");
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.BLUE);
+        towerCanvas = new TowerCanvas();
+        jTextField = new JTextField("3", 2);
+        JButton  addButton = new JButton("add");
+        JButton resetButton = new JButton("reset");
+        JButton playButton = new JButton("play");
 
+        panel.add(jTextField);
+        panel.add(addButton);
+        panel.add(resetButton);
+        panel.add(playButton);
+        
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addAction();
+            }
+        });
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                towerCanvas.reset();
+            }
+        });
+        playButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                towerCanvas.play();
+            }
+        });
+
+        add(towerCanvas, BorderLayout.CENTER);
+        add(panel, BorderLayout.NORTH);
+        setSize(1000, 1000);
+        setLocation(200, 150);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    private void addAction() {
+
+          try{
+              int c  = Integer.parseInt(jTextField.getText());
+              if (c > 7){
+                  JOptionPane.showMessageDialog(null,"please write to number smoll than 7");
+              }else {
+                  towerCanvas.addDisc(c);
+              }
+          }catch (NumberFormatException e){
+              e.printStackTrace();
+              JOptionPane.showMessageDialog(null,"please write to number");
+          }
     }
 
     public static void main(String[] args) {
         TowerFrame towerFrame = new TowerFrame();
-        System.out.println("hello world");
-    }
+
+           }
 }
