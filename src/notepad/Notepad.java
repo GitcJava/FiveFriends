bd3c7eaf06d26237d5e1eabe3e07cdc9c592681f
Grigor <grigorabroyan@gmail.com>
2016-04-30 21:26:45 +0400

    Changed is Notepad: add NotepadMenuBar class

--- src/notepad/Notepad.java
+++ src/notepad/Notepad.java
@@ -154,6 +154,7 @@ public class Notepad extends JFrame {
                 handleDocumentUpdate();
             }
 
+            
             @Override
             public void removeUpdate(DocumentEvent e) {
                 handleDocumentUpdate();
